ae4c66a66e30c0425f402bb959251e5c4d114f0c
SLO-1c-2 <81420686+SLO-1c-2@users.noreply.github.com>
2022-03-30 07:45:37 +0200

    Create 2011_pp_zadanie_4_c.cpp

---
+++ 2011_pp_zadanie_4_c.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    ifstream f("hasla.txt");
+    int suma;
+    while(f>>)
+    {
+        
+    
+    for(int i=0; i<s.size(); i++)
+    {
+        suma = 0;
+        suma = (int)s[i] + (int)s[i+1];
+        if(suma==220) 
+        {
+            
+            
+            cout <<s <<"\n";
+            break;
+        }
+}
+}
+}
